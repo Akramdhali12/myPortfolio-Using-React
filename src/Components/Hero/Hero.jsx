fc364eb3e80d6071ecee446bb5dd7f17b5951e8b
Akram Hossain <dhaliakram6@gmail.com>
2024-10-18 23:42:01 +0800

    add resume

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,6 +4,12 @@ import profile_pic from '../../assets/profile_pic.jpg';
 import {Link} from 'react-scroll'
 
 const Hero = () => {
+  const openInNewTab=()=> {
+    var win = window.open('https://publuu.com/flip-book/691386/1538273', '_blank');
+    if (win != null) {
+      win.focus();
+    }
+  }
   return (
     <div id='home' className='hero'>
         <img src={profile_pic} alt="" />
@@ -11,7 +17,7 @@ const Hero = () => {
         <p>I am a frontend developer from Dhaka,BD with 4 years of experience.</p>
         <div className="hero-action">
             <div className="hero-connect"><Link to='contact' spy={true} smooth={true} offset={100}>Connect with me</Link></div>
-            <div className="hero-resume">My resume</div>
+            <div className="hero-resume" onClick={openInNewTab}>My resume</div>
         </div>
     </div>
   )
